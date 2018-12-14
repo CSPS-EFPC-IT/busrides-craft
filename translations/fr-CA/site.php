dfa0aec98f6ecd1a719ea208dee014882f36bfd7
Julien Lambert-Charette <julien.lambertcharette@gmail.com>
2018-12-13 23:41:46 -0500

    Initial Commit

---
+++ translations/fr-CA/site.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+  'Canada School of Public Service' => 'École de la fonction publique du Canada',
+  'Government of Canada'            => 'Gouvernement du Canada',
+  'Latest Episode'                  => 'Épisode récent',
+  'Other Episodes'                  => 'Autres épisodes',
+  'Play Video'                      => 'Vidéo',
+  'Play Audio'                      => 'Audio',
+  'Read more'                       => 'Lire la suite',
+  'Search'                          => 'Rechercher',
+  'Start searching!'                => 'Lancer la recherche!',
+  'by'                              => 'par',
+];
